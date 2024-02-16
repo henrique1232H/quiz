4500068cb9f7df6c4c3b81a5fdbc82a076ead97b
henrique1232H <henriqueau111@gmail.com>
2024-02-15 21:21:47 -0300

    add questions from quiz and randomize questions

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import quizQuestion from "./quizQuestion.js";
+import randomNumber from "./randomNumber.js";
+
+
+const teste = randomNumber(quizQuestion.length)
+
+console.log(quizQuestion[teste])
---
+++ src/js/randomNumber.js
@@ -0,0 +1,3 @@
+export default function randomNumber(Questions) {
+    return Math.floor(Math.random() * (Questions - 0) - 0 )
+}
