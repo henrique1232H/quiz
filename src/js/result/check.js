3f6548265c9f56854709b8d3a60c0d2b64116f3a
henrique1232H <henriqueau111@gmail.com>
2024-02-25 23:23:06 -0300

    finish

--- src/js/result/check.js
+++ src/js/result/check.js
@@ -2,32 +2,28 @@ import { reset } from "../quiz/showQuestion.js";
 import createLiFromResult from "./createLiFromResult.js";
 
 export default function ifQuizComplete(quiz, answers) {
-  console.log(quiz.length, answers.length);
-
   if (quiz.length === answers.length) {
     reset();
 
     let value = 0;
-    answers.forEach(entries => {
-        
-        createLiFromResult(entries.title, entries.correct)
+    answers.forEach((entries) => {
+      createLiFromResult(entries.title, entries.correct);
 
-        if(entries.correct === "true") {
-            value++
-        }
-
-    })
+      if (entries.correct === "true") {
+        value++;
+      }
+    });
 
     document.querySelector(".result").classList.remove("hiddenAndOpacity");
 
-
-    document.querySelector(".title span").textContent = `${quiz.length}/${value}`
-
+    document.querySelector(
+      ".title span"
+    ).textContent = `${quiz.length}/${value}`;
 
     document.querySelector(".result button").onclick = () => {
-        document.querySelector(".result").classList.add("hiddenAndOpacity");
-        document.querySelector(".result ul").innerHTML = ""
-    }
+      document.querySelector(".result").classList.add("hiddenAndOpacity");
+      document.querySelector(".result ul").innerHTML = "";
+    };
     return;
   }
 }
